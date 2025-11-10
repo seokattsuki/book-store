c8875f41b337fb0470f8ca1c434f3473380580c1
seokattsuki <info.rohitpatra@gmail.com>
2025-11-11 00:04:27 +0530

    Api integrated

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './features/cart/cartSlice'
+import booksApi from './features/books/bookApi'
 
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer
+    cart: cartReducer,
+    [booksApi.reducerPath]: booksApi.reducer
   },
-
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware),
 })
--- Frontend/src/routers/router.jsx
+++ Frontend/src/routers/router.jsx
@@ -5,6 +5,7 @@ import Login from "../components/Login.jsx";
 import Register from "../components/Register.jsx";
 import CartPage from "../pages/books/CartPage.jsx";
 import CheckoutPage from "../pages/books/CheckoutPage.jsx";
+import SingleBook from "../pages/books/SingleBook.jsx";
 
 const router = createBrowserRouter([
     {
@@ -38,6 +39,10 @@ const router = createBrowserRouter([
     {
         path: "/checkout",
         element: <CheckoutPage />
+    },
+    {
+        path: "/books/:id",
+        element: <SingleBook />
     }
 ]
 },
